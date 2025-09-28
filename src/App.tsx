4409546883baf0b47e21a5700b620e1556b22e98
Fellipe Neves <fellipe2.0e@gmail.com>
2025-09-28 18:28:29 -0300

    config: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { RouterProvider } from "react-router";
+import { router } from "./pages/routes";
 
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <RouterProvider router={router} />
+    </>
   )
 }
